8a8b6c14cd01d8e622511eba774e1f7c9ae00632
rayyanabie5-lgtm <rayyanabie5@gmail.com>
2025-12-10 12:28:13 +0700

    Abie 1

---
+++ CaseMethodBeasiswa.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CaseMethodBeasiswa {
+    static Scanner sc = new Scanner(System.in);
+    static final int MAX = 100;
+    static String[] nama = new String[MAX];
+    static String[] nim = new String[MAX];
+    static double[] ipk = new double[MAX];
+    static String[] jenis = new String[MAX];
+    static int[] penghasilan = new int[MAX];
+
+    static int jumlahData = 0;
